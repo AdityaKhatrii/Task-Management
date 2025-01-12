820eaf44efd46dcb3ff75583e605c4c1decb2f72
Aditya Khatri <adityakhatri6038@gmail.com>
2025-01-12 22:16:18 +0530

    ...

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -1,47 +1,102 @@
 import prisma from "../model/prisma";
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import jsonwebtoken, { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt"
+const jwtSecret: string = process.env.JWT_SECRET ? process.env.JWT_SECRET : "";
+const saltRounds: number = 10;
 
-export const registerUser = async(req:Request,res:Response):Promise<any>=>{
+export const registerUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        const {email,password} = req.body;
+        const { email, password } = req.body;
+        const hashedPassword: string = await bcrypt.hash(password, saltRounds);
         const newUser = await prisma.user.create({
-            data: { email, password },
-          });
-          res.status(201).json(newUser);
+            data: { email, password: hashedPassword },
+        });
+        res.status(201).json(newUser);
 
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            error:"Error creating user"
+            error: "Error creating user"
         })
     }
 }
+export type userType = {
+    id: number,
+    email: string,
+    password: string,
+    tasks?: any
+}
 
-export const signInUser = async(req:Request,res:Response):Promise<any>=>{
+export const signInUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        const {email,password} = req.body;
-        const user = await prisma.user.findFirst({where:{
-            email,
-        }});
-        if(!user){
+        const { email, password } = req.body;
+        const user: userType | null = await prisma.user.findFirst({
+            where: {
+                email,
+            }
+        });
+        if (!user) {
             return res.status(404).json({
                 error: 'User not found invalid email'
             });
         }
-        if(user.password != password ){
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) { return res.status(404).json({ error: 'Incorrect password' }); }
+        if (user.password != password) {
             return res.status(404).json({
-                error:'incorrect password '
+                error: 'incorrect password '
             });
         }
-        
+
+        const token: string = jsonwebtoken.sign({
+            email: user.email,
+            id: user.id
+        }, jwtSecret,
+            { expiresIn: '1hr' });
+
         res.status(200).json({
-            message: 'Sign-in successful', user
+            message: 'log in successful', user,
+            token: token
         })
-        
+
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            error:"Signin Error "
+            error: "Signin Error "
+        })
+    }
+}
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const user: userType = req.body.user;
+        const isUserExist = await prisma.user.findFirst( {
+            where:{
+                email:user.email,
+                id: user.id
+            }
+           } );
+        if(!isUserExist){
+            return res.status(400).json({
+                error: "user not found invalid data"
+            })
+        }
+        const isDeleted = await prisma.user.delete({
+            where: {
+                id: user.id
+            }
+        });
+        if (!isDeleted) {
+            throw new Error("db error");
+            }
+        return res.status(200).json({
+            msg:"user deleted successfully"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({
+            error:"Error unable to delete user"
         })
     }
 }
---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,41 @@
+import jsonwebtoken from "jsonwebtoken";
+import prisma from "../model/prisma";
+import { Request, Response, NextFunction } from "express";
+import { userType } from "../controller/userController";
+
+const jwtSecret: string = process.env.JWT_SECRET ? process.env.JWT_SECRET : "";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authHeader: string = req.headers.authorization ? req.headers.authorization : "";
+        if(authHeader.length == 0)
+        {
+            return res.status(401).json({
+                error:"invalid or expired token"
+            })
+        }
+        const token: string = authHeader.split(" ")[1];
+        const user2 = jsonwebtoken.verify(token, jwtSecret,(err,decoded)=>{
+            if (err){
+                return res.status(401).json({err:'Invalid token please sign in again'})
+            }
+            else {
+                console.log(decoded);
+                if (typeof decoded === 'object' && decoded !== null) 
+                    { 
+                        req.body.user = decoded as userType;
+                        next();
+                    }
+                    else 
+                    { return res.status(403).json({ error: 'Token decoding error' });
+            }}
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(403).json({
+            error: "Auth error try to sign in again"
+        })
+    }
+
+}
